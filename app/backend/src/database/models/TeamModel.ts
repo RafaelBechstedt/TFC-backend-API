f9072434d127455cf50b40f030d4fee0df7a3a4b
Rafael <bechstedt@hotmail.com>
2023-05-23 01:19:12 -0300

    Req 01

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,28 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  underscored: true,
+  timestamps: false,
+});
+
+export default Team;
